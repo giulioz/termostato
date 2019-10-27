78c4ae33f15dbb6dcc922c319a00dc0d13c4b158
Giulio Zausa <mail.zausa.giulio@gmail.com>
2019-10-27 18:14:08 +0100

    First interface draft

---
+++ web/pages/index.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+async function fetchJson(url: string) {
+  const data = await fetch(url);
+  return data.json();
+}
+
+export default function Index() {
+  const [currentTemp, setCurrentTemp] = useState("");
+  const [currentActive, setCurrentActive] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      const temp = await fetchJson("iot-gateway:512/stats/temp/current");
+      setCurrentTemp(temp);
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <p>Current temp: {currentTemp}</p>
+    </>
+  );
+}
